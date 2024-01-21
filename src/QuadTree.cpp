e9be6017df616ab4cecf33bc3daf02e3da9b59f6
Jakob <jakob.g.maier@gmail.com>
2024-01-21 10:26:22 +0100

    update

--- src/QuadTree.cpp
+++ src/QuadTree.cpp
@@ -2,16 +2,44 @@
 
 QuadTree::QuadTree(const glm::vec2& min, const glm::vec2& max) { m_root = std::make_unique<Node>(min, max); }
 
-void QuadTree::build_around_point(const glm::vec2& point) { insert(m_root, point); }
+void QuadTree::insert(const glm::vec2& point) { insert(m_root, point); }
 
-std::vector<Node*> QuadTree::get_children() const { return {get_root()}; }
-
-void QuadTree::insert(std::unique_ptr<Node>& child, const glm::vec2& position)
+std::vector<Node*> QuadTree::get_children()
 {
-  float distance_to_child = glm::length(child->center() - position);
+  std::vector<Node*> children;
+  collect(m_root, children);
+  return children;
+}
 
-  if (distance_to_child < child->size().x && child->size().x > MIN_NODE_SIZE) {
+void QuadTree::insert(std::unique_ptr<Node>& node, const glm::vec2& point)
+{
+  float distance_to_child = glm::distance(node->center(), point);
 
+  if (distance_to_child < node->size().x && node->size().x >= MIN_NODE_SIZE) {
+    split_four_ways(node);
+
+    for (std::unique_ptr<Node>& child : node->children) {
+      insert(child, point);
+    }
   }
 }
 
+void QuadTree::split_four_ways(std::unique_ptr<Node>& node)
+{
+  auto min = node->min, max = node->max;
+  auto midpoint = node->center();
+  node->children[0] = std::make_unique<Node>(min, midpoint);
+  node->children[1] = std::make_unique<Node>(min, max);
+  node->children[2] = std::make_unique<Node>(midpoint, max);
+  node->children[3] = std::make_unique<Node>(min, max);
+  node->is_leaf = false;
+}
+
+void QuadTree::collect(std::unique_ptr<Node>& node, std::vector<Node*>& children)
+{
+  if (node->is_leaf) {
+    children.push_back(node.get());
+  } else {
+    for (auto& child : node->children) collect(child, children);
+  }
+}
--- src/QuadTree.h
+++ src/QuadTree.h
@@ -13,16 +13,6 @@ struct Bounds {
   glm::vec2 size() const { return max - min; }
   glm::vec2 half_size() const { return size() / 2.0f; }
   glm::vec2 center() const { return min + half_size(); }
-  std::vector<Bounds> split_four_ways() const
-  {
-    auto midpoint = center();
-    return {
-        Bounds(min, midpoint),
-        Bounds(midpoint, max),
-        Bounds(glm::vec2(min.x, min.y), glm::vec2(max.x, max.y)),
-        Bounds(glm::vec2(min.x, min.y), glm::vec2(max.x, max.y)),
-    };
-  }
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Bounds& b) { return os << b.min << ", " << b.max; }
@@ -31,19 +21,22 @@ enum Direction : size_t { NORTH = 0, SOUTH = 1, EAST = 2, WEST = 3, NUM = 4 };
 
 struct Node : public Bounds {
   Node(const glm::vec2& min_, const glm::vec2& max_) : Bounds(min_, max_) {}
-  std::array<std::unique_ptr<Node>, 4> children;
+  bool is_leaf{true};
+  std::array<std::unique_ptr<Node>, 4> children = {nullptr};
 };
 
 class QuadTree
 {
  public:
   QuadTree(const glm::vec2& min, const glm::vec2& max);
-  void build_around_point(const glm::vec2& point);
-  std::vector<Node*> get_children() const;
+  void insert(const glm::vec2& point);
+  std::vector<Node*> get_children() ;
   Node* get_root() const { return m_root.get(); }
 
  private:
+  const float MIN_NODE_SIZE{0.25f};
   std::unique_ptr<Node> m_root{nullptr};
-  void insert(std::unique_ptr<Node>& child, const glm::vec2& position);
-  const float MIN_NODE_SIZE{.25f};
+  void split_four_ways(std::unique_ptr<Node>& node);
+  void insert(std::unique_ptr<Node>& child, const glm::vec2& point);
+  void collect(std::unique_ptr<Node>& node, std::vector<Node*>& children);
 };
