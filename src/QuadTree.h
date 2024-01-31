de0b21c29ae4fdf973e7c6f881019046b040bc6d
Jakob <45669207+gue-ni@users.noreply.github.com>
2024-01-31 08:10:50 +0100

    Update QuadTree.h

--- src/QuadTree.h
+++ src/QuadTree.h
@@ -13,12 +13,18 @@ struct Node {
   std::array<std::unique_ptr<Node>, 4> children;
 
   Node(const glm::vec2& min_, const glm::vec2& max_, unsigned depth_ = 0) : min(min_), max(max_), depth(depth_) {}
-  glm::vec2 size() const { return max - min; }
-  glm::vec2 center() const { return min + size() / 2.0f; }
-  bool contains(const glm::vec2& point)
+  inline glm::vec2 size() const { return max - min; }
+  inline glm::vec2 center() const { return min + size() / 2.0f; }
+  inline bool contains(const glm::vec2& point)
   {
     return glm::all(glm::lessThanEqual(min, point)) && glm::all(glm::lessThanEqual(point, max));
   }
+
+  // TODO: confirm
+  inline Node* NW() { return is_leaf ? nullptr : children[0].get(); }
+  inline Node* NE() { return is_leaf ? nullptr : children[1].get(); }
+  inline Node* SW() { return is_leaf ? nullptr : children[2].get(); }
+  inline Node* SE() { return is_leaf ? nullptr : children[3].get(); }
 };
 
 class QuadTree
