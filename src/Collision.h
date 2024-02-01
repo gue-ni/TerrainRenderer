09939b9089f08f42c02f412773aee39ffdf8296f
Jakob <45669207+gue-ni@users.noreply.github.com>
2024-02-01 15:27:20 +0100

    Update Collision.h

--- src/Collision.h
+++ src/Collision.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <array>
 
 using Point = glm::vec3;
 
@@ -23,13 +24,17 @@ struct Sphere {
 struct Plane {
   glm::vec3 normal;  // plane normal
   float distance;  // distance from origin     
-  Plane(const glm::vec3 &normal_, const glm::vec3 &point_) : normal(normal_), distance(glm::dot(normal, point)) {}
+  Plane(const glm::vec3 &normal_, const glm::vec3 &point_) : normal(normal_), distance(glm::dot(normal_, point_)) {}
 };
 
 // A segment is a part of a straigt line bounded by two end points.
 struct Segment {
   glm::vec3 a, b;
   inline glm::vec3 point_at(float t) const { return a + (b - a) * t; }
+};
+
+struct Frustum {
+  std::array<Plane, 6> planes;
 };
 
 // Real Time Collison Detection - Christer Ericson, Page
