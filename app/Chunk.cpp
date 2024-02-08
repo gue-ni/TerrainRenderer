d449d01a602c5d360d1d2a0106a93e115024f3d9
Jakob <jakob.g.maier@gmail.com>
2024-02-08 20:53:41 +0100

    update

--- app/Chunk.cpp
+++ app/Chunk.cpp
@@ -76,10 +76,10 @@ Chunk::Chunk(unsigned vertex_count, float size)
   m_vao->bind();
 
   m_vbo->bind();
-  m_vbo->buffer_data(gfx::span(vertices));
+  m_vbo->buffer_data(std::span(vertices));
 
   m_ebo->bind();
-  m_ebo->buffer_data(gfx::span(vertices));
+  m_ebo->buffer_data(std::span(vertices));
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, pos)));
   glEnableVertexAttribArray(0);
